32fd2948a17222c26b3648ea71456ec9827fb259
Josiah <jcmcclurg@gmail.com>
2015-08-20 15:55:41 -0500

    Added installer script

---
+++ installModules.sh
@@ -0,0 +1,4 @@
+git clone git://github.com/wikimedia/puppet-cdh.git /etc/puppet/modules/cdh
+puppet module install puppetlabs-apt
+puppet module install puppetlabs-stlib
+puppet module install puppetlabs-vcsrepo
